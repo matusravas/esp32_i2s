6565927dec1f321aeeb72be0abe21b7f1649a417
Matus Ravas <matustabasco@gmail.com>
2020-11-14 20:52:00 +0100

    project structured to different files - working

--- src/main.cpp
+++ src/main.cpp
@@ -1,138 +1,11 @@
 #include <Arduino.h>
 #include <driver/i2s.h>
 #include <SPIFFS.h>
-
-#define I2S_WS 15
-#define I2S_SD 13
-#define I2S_SCK 2
-#define I2S_PORT I2S_NUM_0
-#define I2S_SAMPLE_RATE   (16000)
-#define I2S_SAMPLE_BITS   (16)
-#define I2S_READ_LEN      (16 * 1024)
-#define RECORD_TIME       (7) //Seconds
-#define I2S_CHANNEL_NUM   (1)
-#define FLASH_RECORD_SIZE (I2S_CHANNEL_NUM * I2S_SAMPLE_RATE * I2S_SAMPLE_BITS / 8 * RECORD_TIME)
-
-File file;
-const char filename[] = "/recording.wav";
-const int headerSize = 44;
+#include "main.h"
+#include "my_fs.h"
 
 
-void wavHeader(byte* header, int wavSize){
-  header[0] = 'R';
-  header[1] = 'I';
-  header[2] = 'F';
-  header[3] = 'F';
-  unsigned int fileSize = wavSize + headerSize - 8;
-  header[4] = (byte)(fileSize & 0xFF);
-  header[5] = (byte)((fileSize >> 8) & 0xFF);
-  header[6] = (byte)((fileSize >> 16) & 0xFF);
-  header[7] = (byte)((fileSize >> 24) & 0xFF);
-  header[8] = 'W';
-  header[9] = 'A';
-  header[10] = 'V';
-  header[11] = 'E';
-  header[12] = 'f';
-  header[13] = 'm';
-  header[14] = 't';
-  header[15] = ' ';
-  header[16] = 0x10;
-  header[17] = 0x00;
-  header[18] = 0x00;
-  header[19] = 0x00;
-  header[20] = 0x01;
-  header[21] = 0x00;
-  header[22] = 0x01;
-  header[23] = 0x00;
-  header[24] = 0x80;
-  header[25] = 0x3E;
-  header[26] = 0x00;
-  header[27] = 0x00;
-  header[28] = 0x00;
-  header[29] = 0x7D;
-  header[30] = 0x00;
-  header[31] = 0x00;
-  header[32] = 0x02;
-  header[33] = 0x00;
-  header[34] = 0x10;
-  header[35] = 0x00;
-  header[36] = 'd';
-  header[37] = 'a';
-  header[38] = 't';
-  header[39] = 'a';
-  header[40] = (byte)(wavSize & 0xFF);
-  header[41] = (byte)((wavSize >> 8) & 0xFF);
-  header[42] = (byte)((wavSize >> 16) & 0xFF);
-  header[43] = (byte)((wavSize >> 24) & 0xFF);
-}
-
-
-void listSPIFFS(void) {
-  Serial.println(F("\r\nListing SPIFFS files:"));
-  static const char line[] PROGMEM =  "=================================================";
-
-  Serial.println(FPSTR(line));
-  Serial.println(F("  File name                              Size"));
-  Serial.println(FPSTR(line));
-
-  fs::File root = SPIFFS.open("/");
-  if (!root) {
-    Serial.println(F("Failed to open directory"));
-    return;
-  }
-  if (!root.isDirectory()) {
-    Serial.println(F("Not a directory"));
-    return;
-  }
-
-  fs::File file = root.openNextFile();
-  while (file) {
-
-    if (file.isDirectory()) {
-      Serial.print("DIR : ");
-      String fileName = file.name();
-      Serial.print(fileName);
-    } else {
-      String fileName = file.name();
-      Serial.print("  " + fileName);
-      // File path can be 31 characters maximum in SPIFFS
-      int spaces = 33 - fileName.length(); // Tabulate nicely
-      if (spaces < 1) spaces = 1;
-      while (spaces--) Serial.print(" ");
-      String fileSize = (String) file.size();
-      spaces = 10 - fileSize.length(); // Tabulate nicely
-      if (spaces < 1) spaces = 1;
-      while (spaces--) Serial.print(" ");
-      Serial.println(fileSize + " bytes");
-    }
-
-    file = root.openNextFile();
-  }
-
-  Serial.println(FPSTR(line));
-  Serial.println();
-  delay(1000);
-}
-
-void SPIFFSInit(){
-  if(!SPIFFS.begin(true)){
-    Serial.println("SPIFFS initialisation failed!");
-    while(1) yield();
-  }
-
-  SPIFFS.format();
-//  SPIFFS.remove(filename);
-  file = SPIFFS.open(filename, FILE_WRITE);
-  if(!file){
-    Serial.println("File is not available!");
-  }
-
-  byte header[headerSize];
-  wavHeader(header, FLASH_RECORD_SIZE);
-
-  file.write(header, headerSize);
-  listSPIFFS();
-}
+extern File file;
 
 
 void i2s_adc_data_scale(uint8_t * d_buff, uint8_t* s_buff, uint32_t len)
@@ -163,11 +36,15 @@ void i2s_adc(void *arg)
     Serial.println(" *** Recording Start *** ");
     while (flash_wr_size < FLASH_RECORD_SIZE) {
         //read data from I2S bus, in this case, from ADC.
-        i2s_read(I2S_PORT, (void*) i2s_read_buff, i2s_read_len, &bytes_read, portMAX_DELAY);
+        ets_printf("t=%ld\n",millis());
+        i2s_read(I2S_PORT, (void*) i2s_read_buff, i2s_read_len, &bytes_read, portMAX_DELAY); //0.5 sec
+        ets_printf("t1=%ld\n",millis());
+        
         //example_disp_buf((uint8_t*) i2s_read_buff, 64);
         //save original data from I2S(ADC) into flash.
         i2s_adc_data_scale(flash_write_buff, (uint8_t*)i2s_read_buff, i2s_read_len);
-        file.write((const byte*) flash_write_buff, i2s_read_len);
+        file.write((const byte*) flash_write_buff, i2s_read_len); //0.1 sec
+        ets_printf("t2=%ld\n",millis());
         flash_wr_size += i2s_read_len;
         ets_printf("Sound recording %u%%\n", flash_wr_size * 100 / FLASH_RECORD_SIZE);
         ets_printf("Never Used Stack Size: %u\n", uxTaskGetStackHighWaterMark(NULL));
@@ -221,72 +98,3 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
-
-
-
-
-
-
-
-//void example_disp_buf(uint8_t* buf, int length)
-//{
-//    printf("======\n");
-//    for (int i = 0; i < length; i++) {
-//        printf("%02x ", buf[i]);
-//        if ((i + 1) % 8 == 0) {
-//            printf("\n");
-//        }
-//    }
-//    printf("======\n");
-//}
-
-
-//void wavHeader(byte* header, int wavSize){
-//  header[0] = 'R';
-//  header[1] = 'I';
-//  header[2] = 'F';
-//  header[3] = 'F';
-//  unsigned int fileSize = wavSize + headerSize - 8;
-//  header[4] = (byte)(fileSize & 0xFF);
-//  header[5] = (byte)((fileSize >> 8) & 0xFF);
-//  header[6] = (byte)((fileSize >> 16) & 0xFF);
-//  header[7] = (byte)((fileSize >> 24) & 0xFF);
-//  header[8] = 'W';
-//  header[9] = 'A';
-//  header[10] = 'V';
-//  header[11] = 'E';
-//  header[12] = 'f';
-//  header[13] = 'm';
-//  header[14] = 't';
-//  header[15] = ' ';
-//  header[16] = 0x10;
-//  header[17] = 0x00;
-//  header[18] = 0x00;
-//  header[19] = 0x00;
-//  header[20] = 0x01;
-//  header[21] = 0x00;
-//  header[22] = 0x01;
-//  header[23] = 0x00;
-//  header[24] = 0x3E;
-//  header[25] = 0x80;
-//  header[26] = 0x00;
-//  header[27] = 0x00;
-//  header[28] = 0x7D;
-//  header[29] = 0x00;
-//  header[30] = 0x00;
-//  header[31] = 0x00;
-//  header[32] = 0x02;
-//  header[33] = 0x00;
-//  header[34] = 0x10;
-//  header[35] = 0x00;
-//  header[36] = 'd';
-//  header[37] = 'a';
-//  header[38] = 't';
-//  header[39] = 'a';
-//  header[40] = (byte)(wavSize & 0xFF);
-//  header[41] = (byte)((wavSize >> 8) & 0xFF);
-//  header[42] = (byte)((wavSize >> 16) & 0xFF);
-//  header[43] = (byte)((wavSize >> 24) & 0xFF);
-//}
-
-
---
+++ src/my_fs.cpp
@@ -0,0 +1,123 @@
+#include <Arduino.h>
+#include "my_fs.h"
+
+
+File file;
+const char filename[] = "/recording.wav";
+const int headerSize = 44;
+
+
+void wavHeader(byte* header, int wavSize){
+  header[0] = 'R';
+  header[1] = 'I';
+  header[2] = 'F';
+  header[3] = 'F';
+  unsigned int fileSize = wavSize + headerSize - 8;
+  header[4] = (byte)(fileSize & 0xFF);
+  header[5] = (byte)((fileSize >> 8) & 0xFF);
+  header[6] = (byte)((fileSize >> 16) & 0xFF);
+  header[7] = (byte)((fileSize >> 24) & 0xFF);
+  header[8] = 'W';
+  header[9] = 'A';
+  header[10] = 'V';
+  header[11] = 'E';
+  header[12] = 'f';
+  header[13] = 'm';
+  header[14] = 't';
+  header[15] = ' ';
+  header[16] = 0x10;
+  header[17] = 0x00;
+  header[18] = 0x00;
+  header[19] = 0x00;
+  header[20] = 0x01;
+  header[21] = 0x00;
+  header[22] = 0x01;
+  header[23] = 0x00;
+  header[24] = 0x80;
+  header[25] = 0x3E;
+  header[26] = 0x00;
+  header[27] = 0x00;
+  header[28] = 0x00;
+  header[29] = 0x7D;
+  header[30] = 0x00;
+  header[31] = 0x00;
+  header[32] = 0x02;
+  header[33] = 0x00;
+  header[34] = 0x10;
+  header[35] = 0x00;
+  header[36] = 'd';
+  header[37] = 'a';
+  header[38] = 't';
+  header[39] = 'a';
+  header[40] = (byte)(wavSize & 0xFF);
+  header[41] = (byte)((wavSize >> 8) & 0xFF);
+  header[42] = (byte)((wavSize >> 16) & 0xFF);
+  header[43] = (byte)((wavSize >> 24) & 0xFF);
+}
+
+void listSPIFFS(void) {
+  Serial.println(F("\r\nListing SPIFFS files:"));
+  static const char line[] PROGMEM =  "=================================================";
+
+  Serial.println(FPSTR(line));
+  Serial.println(F("  File name                              Size"));
+  Serial.println(FPSTR(line));
+
+  fs::File root = SPIFFS.open("/");
+  if (!root) {
+    Serial.println(F("Failed to open directory"));
+    return;
+  }
+  if (!root.isDirectory()) {
+    Serial.println(F("Not a directory"));
+    return;
+  }
+
+  fs::File file = root.openNextFile();
+  while (file) {
+
+    if (file.isDirectory()) {
+      Serial.print("DIR : ");
+      String fileName = file.name();
+      Serial.print(fileName);
+    } else {
+      String fileName = file.name();
+      Serial.print("  " + fileName);
+      // File path can be 31 characters maximum in SPIFFS
+      int spaces = 33 - fileName.length(); // Tabulate nicely
+      if (spaces < 1) spaces = 1;
+      while (spaces--) Serial.print(" ");
+      String fileSize = (String) file.size();
+      spaces = 10 - fileSize.length(); // Tabulate nicely
+      if (spaces < 1) spaces = 1;
+      while (spaces--) Serial.print(" ");
+      Serial.println(fileSize + " bytes");
+    }
+
+    file = root.openNextFile();
+  }
+
+  Serial.println(FPSTR(line));
+  Serial.println();
+  delay(1000);
+}
+
+void SPIFFSInit(){
+  if(!SPIFFS.begin(true)){
+    Serial.println("SPIFFS initialisation failed!");
+    while(1) yield();
+  }
+
+  SPIFFS.format();
+//  SPIFFS.remove(filename);
+  file = SPIFFS.open(filename, FILE_WRITE);
+  if(!file){
+    Serial.println("File is not available!");
+  }
+
+  byte header[headerSize];
+  wavHeader(header, FLASH_RECORD_SIZE);
+
+  file.write(header, headerSize);
+  listSPIFFS();
+}
